30d51a715513c559566cd7b44268f9a2b99da487
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-12-16 19:35:47 +0200

    refactor: Change main padding variable and add header component with navigation for large devices

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,3 @@
+export { HeaderComponent } from "./header/header.component";
 export { FindMeOnSocialComponent } from "./find-me-on-social/find-me-on-social.component";
 export { FooterComponent } from "./footer/footer.component";
