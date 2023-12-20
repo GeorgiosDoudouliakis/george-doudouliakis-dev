c84fe0532348e3fed011e8429aa6a7e482f080f3
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-12-20 02:02:49 +0200

    refactor: Add skills on home intro

--- src/app/pages/home/components/home-intro/home-intro.component.ts
+++ src/app/pages/home/components/home-intro/home-intro.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { NgOptimizedImage } from "@angular/common";
 import { TypeWriterDirective } from "../../directives/type-writer.directive";
 
 @Component({
   selector: 'gd-home-intro',
   standalone: true,
-  imports: [TypeWriterDirective, RouterLink],
+  imports: [TypeWriterDirective, RouterLink, NgOptimizedImage],
   templateUrl: './home-intro.component.html',
   styleUrl: './home-intro.component.scss'
 })
