521d3d3049a19fff03a8a631c29945a7257cf9b9
Georgios Doudouliakis <dudugeorge89@gmail.com>
2024-01-01 22:48:09 +0200

    refactor: Add the layout on contact page

--- src/app/pages/contact/layout/contact-layout.component.ts
+++ src/app/pages/contact/layout/contact-layout.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'gd-contact-layout',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact-layout.component.html',
   styleUrl: './contact-layout.component.scss'
 })
-export class ContactLayoutComponent {
+export class ContactLayoutComponent implements OnInit {
+  public contactForm: FormGroup;
 
+  constructor(private _fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this._initializeContactForm();
+  }
+
+  private _initializeContactForm(): void {
+    this.contactForm = this._fb.group({
+      name: this._fb.control<string>("", { nonNullable: true, validators: [Validators.required] }),
+      email: this._fb.control<string>("", { nonNullable: true, validators: [Validators.required, Validators.email] }),
+      subject: this._fb.control<string>("", { nonNullable: true, validators: [Validators.required] }),
+      message: this._fb.control<string>("", { nonNullable: true, validators: [Validators.required] }),
+    });
+  }
 }
