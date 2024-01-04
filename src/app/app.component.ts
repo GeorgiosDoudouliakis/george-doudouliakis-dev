55b7023dcadb2db028ad5919ccf8708808402567
Georgios Doudouliakis <dudugeorge89@gmail.com>
2024-01-04 22:11:00 +0200

    refactor: Delete social media component and social media images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent, SocialMediaComponent, FooterComponent } from "./components";
+import { FooterComponent, HeaderComponent } from "./components";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, SocialMediaComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,5 @@
-export { HeaderComponent } from "./header/header.component";
-export { SocialMediaComponent } from "./social-media/social-media.component";
 export { FooterComponent } from "./footer/footer.component";
-export { NonMobileNavigationComponent } from "./non-mobile-navigation/non-mobile-navigation.component";
+export { HeaderComponent } from "./header/header.component";
 export { MobileNavigationComponent } from "./mobile-navigation/mobile-navigation.component";
+export { NonMobileNavigationComponent } from "./non-mobile-navigation/non-mobile-navigation.component";
+
