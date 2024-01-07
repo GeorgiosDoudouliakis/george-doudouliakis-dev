c689bad5554e56e6391250073f405f4372e6ad40
Georgios Doudouliakis <dudugeorge89@gmail.com>
2024-01-07 16:14:40 +0200

    refactor: Add body color as a background for secondary button class and get in touch and show cv buttons on home intro

--- src/app/pages/home/components/home-intro/home-intro.component.ts
+++ src/app/pages/home/components/home-intro/home-intro.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 import { TypeWriterDirective } from "../../directives/type-writer.directive";
 
 @Component({
   selector: 'gd-home-intro',
   standalone: true,
-  imports: [TypeWriterDirective],
+  imports: [TypeWriterDirective, RouterLink],
   templateUrl: './home-intro.component.html',
   styleUrl: './home-intro.component.scss'
 })
