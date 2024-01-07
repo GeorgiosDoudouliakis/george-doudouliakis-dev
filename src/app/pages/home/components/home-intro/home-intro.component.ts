e6144ea0631779ecee64493d7853591622abc78f
Georgios Doudouliakis <dudugeorge89@gmail.com>
2024-01-07 15:12:25 +0200

    refactor: Remove about me button on home intro

--- src/app/pages/home/components/home-intro/home-intro.component.ts
+++ src/app/pages/home/components/home-intro/home-intro.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
-import { NgOptimizedImage } from "@angular/common";
 import { TypeWriterDirective } from "../../directives/type-writer.directive";
 
 @Component({
   selector: 'gd-home-intro',
   standalone: true,
-  imports: [TypeWriterDirective, RouterLink, NgOptimizedImage],
+  imports: [TypeWriterDirective],
   templateUrl: './home-intro.component.html',
   styleUrl: './home-intro.component.scss'
 })
