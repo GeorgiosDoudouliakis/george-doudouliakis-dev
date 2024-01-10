7ec352653b8b2d8f4cc27f0f11b0524c3ad06d85
Georgios Doudouliakis <dudugeorge89@gmail.com>
2024-01-11 00:31:20 +0200

    refactor: Change route titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,27 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: "",
+    title: "George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/home/layout/home.component").then(({ HomeComponent }) => HomeComponent)
   },
   {
     path: "about",
+    title: "About | George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/about/layout/about.component").then(({ AboutComponent }) => AboutComponent),
   },
   {
     path: "experience",
+    title: "Experience | George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/experience/layout/experience.component").then(({ ExperienceComponent }) => ExperienceComponent),
   },
   {
     path: "projects",
+    title: "Projects | George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/projects/layout/projects.component").then(({ ProjectsComponent }) => ProjectsComponent),
   },
   {
     path: "contact",
+    title: "Contact | George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/contact/layout/contact.component").then(({ ContactComponent }) => ContactComponent),
   }
 ];
