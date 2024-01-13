79adce922b0cbf00d95538c9f673b033d60d839e
Georgios Doudouliakis <dudugeorge89@gmail.com>
2024-01-13 14:11:01 +0200

    refactor: Change mobile menu styles and on toggle mobile menu change overflow of body

--- src/app/components/mobile-navigation/mobile-navigation.component.ts
+++ src/app/components/mobile-navigation/mobile-navigation.component.ts
@@ -8,12 +8,21 @@ import { NonMobileNavigationComponent } from "../non-mobile-navigation/non-mobil
   standalone: true,
   imports: [NgClass, RouterLink, RouterLinkActive, NonMobileNavigationComponent],
   templateUrl: './mobile-navigation.component.html',
-  styleUrl: './mobile-navigation.component.scss'
+  styleUrl: './mobile-navigation.component.scss',
+  providers: [
+    {
+      provide: Window,
+      useValue: window
+    }
+  ]
 })
 export class MobileNavigationComponent {
   public isMobileMenuActive: WritableSignal<boolean> = signal<boolean>(false);
 
+  constructor(private _window: Window) {}
+
   public toggleMobileMenu(): void {
     this.isMobileMenuActive.set(!this.isMobileMenuActive());
+    this._window.document.body.style.overflow = this.isMobileMenuActive() ? "hidden" : "auto";
   }
 }
